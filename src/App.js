e9c161999321525530ace4da45c7771fe754660e
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-10-30 11:10:05 +0200

    add url on git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Viktoriia Zelenova and is{" "}
+        <a
+          href="https://github.com/ZelenovaV/weather-react2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on a GitHub
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
