2f84071eceedcab049c8bdcb34dcd1da2b9e7bc2
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-10-30 12:17:20 +0200

    add css file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Viktoriia Zelenova and is{" "}
-        <a
-          href="https://github.com/ZelenovaV/weather-react2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on a GitHub
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Viktoriia Zelenova and is{" "}
+          <a
+            href="https://github.com/ZelenovaV/weather-react2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on a GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+export default function () {
+  return (
+    <>
+      <div className="Weather">Hello</div>
+      <h1>Dnipro</h1>
+      <ul>
+        <li>sunday, 12.00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+    </>
+  );
+}
