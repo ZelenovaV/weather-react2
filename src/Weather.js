5564153da77edb082c1a2488f38a6d254bb92b93
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-10-30 15:04:44 +0200

    add css

--- src/Weather.js
+++ src/Weather.js
@@ -6,27 +6,44 @@ export default function Weather() {
     <>
       <div className="Weather">
         <form>
-          <input
-            type="search"
-            placeholder="Search a city"
-            className="form-control"
-          />
-          <input type="submit" value="Search" className="btn" />
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
         <h1>Dnipro</h1>
         <ul>
           <li>sunday, 12.00</li>
           <li>Mostly Cloudy</li>
         </ul>
-        <div className="row">
-          <div className="col">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy"
-            />{" "}
-            6 C
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="cloudy"
+                className="float-left"
+              />{" "}
+              <div className="float-left">
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
-          <div className="col">
+          <div className="col-6">
             <ul>
               <li>Precipitation 15%</li>
               <li>Humidity: 72$</li>
